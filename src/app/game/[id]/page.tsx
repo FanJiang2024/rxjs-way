1e94f188eee2a0c363297dfaf5d27ac7e141acc2
FanJiang <fanjiang233@proton.me>
2024-04-29 00:39:26 +0800

    bug fix

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -2,11 +2,15 @@
 
 import GameView, { GameViewId } from "@/components/GameView";
 import { notFound } from "next/navigation";
+import React from "react";
 
-export default function Page({ params }: { params: { id: string } }) {
+function Page({ params }: { params: { id: string } }) {
   const isGame = ["mineSweepers", "snake", "tetris"].includes(params.id);
   if (!isGame) {
     notFound();
   }
   return <GameView id={params.id as GameViewId} />;
 }
+
+const MemoPage = React.memo(Page);
+export default MemoPage;
